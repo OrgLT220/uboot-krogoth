02585a0a60d9f471581f22efedb462a6b29df383
IgnasiSoler <ignasi.soler.flores@gmail.com>
2018-03-13 11:13:34 +0100

    Added boot from QSPI, but it does not boot

--- drivers/spi/fsl_qspi.c
+++ drivers/spi/fsl_qspi.c
@@ -189,32 +189,32 @@
 #endif
 
 /* Flash opcodes. */
-#define	OPCODE_WREN		0x06	/* Write enable */
-#define	OPCODE_RDSR		0x05	/* Read status register */
-#define	OPCODE_WRSR		0x01	/* Write status register 1 byte */
-#define	OPCODE_NORM_READ	0x03	/* Read data bytes (low frequency) */
-#define	OPCODE_FAST_READ	0x0b	/* Read data bytes (high frequency) */
+#define	OPCODE_WREN				0x06	/* Write enable */
+#define	OPCODE_RDSR				0x05	/* Read status register */
+#define	OPCODE_WRSR				0x01	/* Write status register 1 byte */
+#define	OPCODE_NORM_READ		0x03	/* Read data bytes (low frequency) */
+#define	OPCODE_FAST_READ		0x0b	/* Read data bytes (high frequency) */
 #define	OPCODE_QUAD_READ        0x6b    /* Read data bytes */
 #define	OPCODE_DDR_QUAD_READ	0x6d    /* Read data bytes in DDR mode*/
-#define	OPCODE_PP		0x02	/* Page program (up to 256 bytes) */
-#define	OPCODE_BE_4K		0x20	/* Erase 4KiB block */
-#define	OPCODE_BE_4K_PMC	0xd7	/* Erase 4KiB block on PMC chips */
-#define	OPCODE_BE_32K		0x52	/* Erase 32KiB block */
-#define	OPCODE_CHIP_ERASE	0xc7	/* Erase whole flash chip */
-#define	OPCODE_SE		0xd8	/* Sector erase (usually 64KiB) */
-#define	OPCODE_RDID		0x9f	/* Read JEDEC ID */
+#define	OPCODE_PP				0x02	/* Page program (up to 256 bytes) */
+#define	OPCODE_BE_4K			0x20	/* Erase 4KiB block */
+#define	OPCODE_BE_4K_PMC		0xd7	/* Erase 4KiB block on PMC chips */
+#define	OPCODE_BE_32K			0x52	/* Erase 32KiB block */
+#define	OPCODE_CHIP_ERASE		0xc7	/* Erase whole flash chip */
+#define	OPCODE_SE				0xd8	/* Sector erase (usually 64KiB) */
+#define	OPCODE_RDID				0x9f	/* Read JEDEC ID */
 #define	OPCODE_RDCR             0x35    /* Read configuration register */
 
 /* 4-byte address opcodes - used on Spansion and some Macronix flashes. */
-#define	OPCODE_NORM_READ_4B	0x13	/* Read data bytes (low frequency) */
-#define	OPCODE_FAST_READ_4B	0x0c	/* Read data bytes (high frequency) */
-#define	OPCODE_QUAD_READ_4B	0x6c    /* Read data bytes */
-#define	OPCODE_PP_4B		0x12	/* Page program (up to 256 bytes) */
-#define	OPCODE_SE_4B		0xdc	/* Sector erase (usually 64KiB) */
+#define	OPCODE_NORM_READ_4B		0x13	/* Read data bytes (low frequency) */
+#define	OPCODE_FAST_READ_4B		0x0c	/* Read data bytes (high frequency) */
+#define	OPCODE_QUAD_READ_4B		0x6c    /* Read data bytes */
+#define	OPCODE_PP_4B			0x12	/* Page program (up to 256 bytes) */
+#define	OPCODE_SE_4B			0xdc	/* Sector erase (usually 64KiB) */
 
 /* Used for SST flashes only. */
-#define	OPCODE_BP		0x02	/* Byte program */
-#define	OPCODE_WRDI		0x04	/* Write disable */
+#define	OPCODE_BP			0x02	/* Byte program */
+#define	OPCODE_WRDI			0x04	/* Write disable */
 #define	OPCODE_AAI_WP		0xad	/* Auto address increment word program */
 
 /* Used for Macronix and Winbond flashes. */
